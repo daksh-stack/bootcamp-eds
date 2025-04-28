817ca08d252f78b9dd9c0025d58e08ea1607ad29
Daksh Hardiya <138387207+daksh-stack@users.noreply.github.com>
2025-04-28 12:34:34 +0530

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-white text-black">
+    <div className=" w-screen bg-white text-black">
       <h1 className="text-[300px] font-bold">Hello World</h1>
     </div>
   );
